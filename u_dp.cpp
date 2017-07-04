12b3a07489dc36e922a5abfcb78ab2b6d28505e5
yeasin Rahaman <ryeashin@gmail.com>
2017-07-04 22:52:06 +0600

    Update u_dp.cpp

--- u_dp.cpp
+++ u_dp.cpp
@@ -1,62 +1,104 @@
+/*Get a suitable solution idiot! */
+
 #include<bits/stdc++.h>
 #define sf scanf 
 #define pf printf
 
 using namespace std;
 
+
+int ranks(string s,string ss)
+{
+	
+	if(s == "pedra" && ss == "lagarto")
+		return 1;
+	else if(ss == "pedra" && s == "lagarto")
+		return 2;
+	
+	if(s == "tesoura" && ss == "papel")
+		return 1;
+	else if(ss == "tesoura" && s == "papel")
+		return 2;
+	
+	if(s == "papel" && ss == "pedra")
+		return 1;
+	else if(ss == "papel" && s == "pedra")
+		return 2;
+	
+	if(s == "lagarto" && ss == "Spock")
+		return 1;
+	else if(s == "lagarto" && ss == "Spock")
+		return 2;
+		
+	if(s == "Spock" && ss == "tesoura")
+		return 1;
+	else if(ss == "Spock" && s == "tesoura")
+		return 2;	
+	
+	if(s == "tesoura" && ss == "lagarto")
+		return 1;
+	else if(ss == "tesoura" && s == "lagarto")
+		return 2;
+		
+	if(s == "lagarto" && ss == "papel")
+		return 1;
+	else if(ss== "lagarto" && s == "papel")
+		return 2;
+		
+	if(s == "papel" && ss == "Spock")
+		return 1;
+	else if(ss == "papel" && s == "Spock")
+		return 2;
+		
+	if(s == "Spock" && ss == "pedra") 
+		return 1;
+	else if(ss == "Spock" && s == "pedra")
+		return 2;
+	
+	if(s == "pedra" && ss == "tesoura")
+		return 1;
+	else if(ss == "pedra" && s == "tesoura")
+		return 2;
+		
+		
+	return 0;
+}
+
 int main()
 {
-	int mat[256][256];
-	int n;
+	//freopen("in.txt","rt",stdin);
+	// freopen("out.txt","wt",stdout);
 	
-	while(sf("%d",&n) != EOF)
+	int n;
+	string s3,ss;
+	sf("%d",&n);
+	
+	for(int i=1; i<=n; i++)
 	{
-		int x=0,y=n-1;
-		for(int i=0; i<n; i++)
+		cin >> s3 >> ss;
+		
+		if(s3 == ss)
 		{
-			for(int j=0; j<n; j++)
-			{
-				mat[i][j] = 0;
-				mat[i][x] = 2;
-				mat[i][y] =3;
-			}
-			x++;y--;
+			pf("Caso #%d: De novo!\n",i);
+			continue;
 		}
 		
-		int res = n/3,half = res+2;
-		//~ cout << res << " " << half << endl;
-		for(int i=res; i<=half; i++)
+		// cout << s3 << " " << ss << endl;
+		
+		int nn = ranks(s3,ss);
+		if(nn==1)
 		{
-			for(int j=res; j<=half; j++)
-			{
-				mat[i][j] = 1;
-				//~ cout << mat[i][j] << " ";
-		
-			}
-			//~ cout<< endl;
+			pf("Caso #%d: Bazinga!\n",i);
 		}
-		mat[n/2][n/2] = 4;
-		
-		
-		for(int i=0; i<n; i++)
+		else if(nn==2)
 		{
-			for(int j=0; j<n; j++)
-			{
-				if(j<n-1)
-				{
-					pf("%d ",mat[i][j]);
-				}
-				else if(j==n-1)
-				{
-					pf("%d\n",mat[i][j]);
-				}
-			}
+			pf("Caso #%d: Raj trapaceou!\n",i);
 		}
-		
-		pf("\n");
+
 	}
+
+	
 	
 	
 	return 0;
 }
-
