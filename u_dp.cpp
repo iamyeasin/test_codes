ad18f7d7675c459330ae715f62b622c509f45f7b
yeasin Rahaman <ryeashin@gmail.com>
2017-07-07 02:19:42 +0600

    Update u_dp.cpp

--- u_dp.cpp
+++ u_dp.cpp
@@ -1,57 +1,119 @@
 #include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
 
 using namespace std;
 
+int dec(char ss)
+{
+    return ss - '0';
+}
+
+int zero(char *sn,int pos)
+{
+    int n = strlen(sn);
+    int xr = 0;
+    for(int i=pos; i<n; i++)
+    {
+        if(sn[i] != '0')break;
+        else if(sn[i] ==0)xr++;
+    }
+
+    return xr;
+}
+
 int main()
 {
-    string s1;
-    getline(cin,s1);
+    char s[1024];
+    char st[1024];
+    gets(s);
 
+    int len = strlen(s);
+    int pos=0,neg=0;
 
-    char st[123];
-    int len = s1.length();
-
-    if(s1[0] == '-') st[0] = '-';
-    else st[0] = '+';
-
-    int flag=0,dot=-5,fz=0;
-    bool fs=0,dt=false;
-
-
-    for(int i=0; i<len; i++)
+    if(s[0] == '-')
     {
-
-        if((s1[i]-'0') > 0 && !fs)
-        {
-            st[1] = s1[i];
-            fs = true;
-
-            st[2] = '.';dt=true;
-        }
-        else if(s1[i] == '.' && !dt)
-        {
-            st[2] = '.';dt = true;
-        }
-
-        if(s1[i] == '.'){ dot=i;dt=true; }
+        st[0] = '-';
+        neg = 1;
     }
-    dot = (dot==-5) ? 0 : dot;
-//    cout << dot << endl;
-
-    int sk = 3;
-
-    for(int i=dot; i<=dot+3; i++)
+    else
     {
-        st[sk++] = s1[i+1];
+        st[0] = '+';
+        pos = 1;
     }
-    st[sk++] = (s1[dot+3]-'0' > 5) ? (s1[dot+3]-'0' +1 ) + '0' : s1[dot+3];
+
+    int ok=0,dPos=0,noPoint=0,ind=1,xr;
+
+    for(int i=0; i<=len; i++)
+    {
+        if(s[i] == '.')
+        {
+            dPos = i;
+            noPoint = 1;
+            break;
+        }
+
+        if(((s[i]-'0') > 0 ) && !ok)
+        {
+            st[ind++] = s[i];
+            ok=1;
+        }
+    }
+
+    if(noPoint)
+    {
+        xr = zero(s,dPos);
+    }
+
+    if(!ok)
+    {
+        pf("%c0000E00\n",st[0]);
+        return 0;
+    }
+
+    //If no point found on the string
+    if(!noPoint)
+    {
+//		cout << len << endl;
+        if(s[0] == '-' || s[0] == '+')
+        {
+            char chs = (dec(st[6])) >= 5 ? dec(st[5])+1+'0' : st[5];
+            if(len>=1 && len<=9)
+            {
+                pf("%c%c.%c%c%c%cE0%d\n",st[0],s[1],s[2],s[3],s[4],s[5],chs,len);
+            }
+            else
+            {
+                pf("%c%c.%c%c%c%cE%d\n",st[0],s[1],s[2],s[3],s[4],s[5],chs,len);
+            }
+
+        }
+        else
+        {
+            char chs = (dec(st[5])) >= 5 ? dec(st[4])+1+'0' : st[4];
+            if(len>=1 && len<=9)
+            {
+                pf("%c%c.%c%c%c%cE0%d\n",st[0],s[0],s[1],s[2],s[3],s[4],chs,len);
+            }
+            else
+            {
+                pf("%c%c.%c%c%c%cE%d\n",st[0],s[0],s[1],s[2],s[3],s[5],chs,len);
+            }
+
+        }
+
+    }
+
+    else // IF there is a point
+    {
+        // copying data
+
+        cout << st << endl;
+
+    }
 
 
 
 
-    cout << st<< endl;
-
-
-
-    return 0;
+     return 0;
 }
