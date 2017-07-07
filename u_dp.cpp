2517e4348fdc00cb47ecfcca6a8b6653999454c0
yeasin Rahaman <ryeashin@gmail.com>
2017-07-07 11:55:08 +0600

    Update u_dp.cpp

--- u_dp.cpp
+++ u_dp.cpp
@@ -13,10 +13,16 @@ int zero(char *sn,int pos)
 {
     int n = strlen(sn);
     int xr = 0;
-    for(int i=pos; i<n; i++)
+    for(int i=0; i<n; i++)
     {
-        if(sn[i] != '0')break;
-        else if(sn[i] ==0)xr++;
+        if(dec(sn[i]) > 0)
+        {
+            break;
+        }
+        else if(sn[i] == '0' )
+        {
+            xr++;
+        }
     }
 
     return xr;
@@ -24,96 +30,172 @@ int zero(char *sn,int pos)
 
 int main()
 {
-    char s[1024];
-    char st[1024];
-    gets(s);
-
-    int len = strlen(s);
-    int pos=0,neg=0;
-
-    if(s[0] == '-')
+    while(1)
     {
-        st[0] = '-';
-        neg = 1;
-    }
-    else
-    {
-        st[0] = '+';
-        pos = 1;
-    }
+        char s[1024];
+        char st[1024];
+        gets(s);
 
-    int ok=0,dPos=0,noPoint=0,ind=1,xr;
+        int len = strlen(s);
+        int pos=0,neg=0;
 
-    for(int i=0; i<=len; i++)
-    {
-        if(s[i] == '.')
+        if(s[0] == '-')
         {
-            dPos = i;
-            noPoint = 1;
-            break;
-        }
-
-        if(((s[i]-'0') > 0 ) && !ok)
-        {
-            st[ind++] = s[i];
-            ok=1;
-        }
-    }
-
-    if(noPoint)
-    {
-        xr = zero(s,dPos);
-    }
-
-    if(!ok)
-    {
-        pf("%c0000E00\n",st[0]);
-        return 0;
-    }
-
-    //If no point found on the string
-    if(!noPoint)
-    {
-//		cout << len << endl;
-        if(s[0] == '-' || s[0] == '+')
-        {
-            char chs = (dec(st[6])) >= 5 ? dec(st[5])+1+'0' : st[5];
-            if(len>=1 && len<=9)
-            {
-                pf("%c%c.%c%c%c%cE0%d\n",st[0],s[1],s[2],s[3],s[4],s[5],chs,len);
-            }
-            else
-            {
-                pf("%c%c.%c%c%c%cE%d\n",st[0],s[1],s[2],s[3],s[4],s[5],chs,len);
-            }
-
+            st[0] = '-';
+            neg = 1;
         }
         else
         {
-            char chs = (dec(st[5])) >= 5 ? dec(st[4])+1+'0' : st[4];
-            if(len>=1 && len<=9)
+            st[0] = '+';
+            pos = 1;
+        }
+
+        int ok=0,dPos=0,noPoint=0,ind=1,xr,fL;
+
+        for(int i=0; i<=len; i++)
+        {
+            if(s[i] == '.')
             {
-                pf("%c%c.%c%c%c%cE0%d\n",st[0],s[0],s[1],s[2],s[3],s[4],chs,len);
+                dPos = i;
+                noPoint = 1;
             }
-            else
+
+            if(((s[i]-'0') > 0 ) && !ok)
             {
-                pf("%c%c.%c%c%c%cE%d\n",st[0],s[0],s[1],s[2],s[3],s[5],chs,len);
+                st[ind++] = s[i];
+                ok=1;
+                fL = i;
             }
+        }
+
+        if(noPoint)
+        {
+            xr = zero(s,dPos);
+        }
+
+        if(!ok)
+        {
+            pf("%c0000E+00\n",st[0]);
+            return 0;
+        }
+        st[ind++] = '.';
+        //If no point found on the string
+        if(!noPoint)
+        {
+            int in = 1;
+            int k = (s[0] == '-' || s[0] == '+') ? 2 : 1;
+            for(int i=k; i<=4; i++)
+            {
+                st[ind++] = i >= len? '0' : s[i];
+            }
+            if(dec(s[5]) > 0) {st[6]= (st[6]-'0')+1+'0';}
+//            cout << ind << endl;
+            if(ind < 6)
+            {
+                for(int i=ind; i<6; i++)
+                {
+                    st[i] = '0';
+                }
+            }
+
+
+            st[ind++] = 'E';
+            st[ind++] = '+';
+
+            int dig=0,flag=0,fuck;
+            int zr = zero(s,k);
+//            cout << zr << endl;
+
+            fuck = zr>0 ? zr : 1;
+
+            cout << fuck << endl;
+            for(int k=len-1; k>=fuck; k--)
+            {
+                if(dec(s[k])>=0 && dec(s[k])<=0)dig++;
+            }
+//
+            cout << dig << endl;
+
+            if(dig >=1 && dig<=9)st[ind++] = '0';
+
+            pf("%s%d\n",st,dig);
 
         }
 
+        else // IF there is a point
+        {
+
+//            st[ind++] = '.';
+            // copying data
+            for(int i=fL+1; i<=fL+4; i++)
+            {
+                if(s[i]=='.')
+                {
+                    fL++;
+                    continue;
+                }
+                else if(i>=len)
+                {
+                    st[ind++] = '0';
+                }
+                else
+                {
+                    st[ind++] = s[i];
+                }
+            }
+
+            st[ind++] = 'E';
+
+            if(xr > 0 ) st[ind++] = '-';
+            else st[ind++] = '+';
+
+            int posCounter=0,sp;
+            sp = (s[0] =='-' || s[0] == '+') ? 2 : 1;
+
+            int k=0;
+//            if(sp == 2)
+            {
+                for(int i=sp-1; i<len; i++)
+                {
+                    if(s[i] == '.')continue;
+                    else if(k == 5)
+                    {
+                        if(dec(s[i]) >= 5)st[6]=(((st[6]-'0')+1)+'0');
+                        break;
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                }
+            }
+
+
+            if(xr > 0)
+            {
+                for(int i=sp; i<len; i++)
+                {
+                    if(s[i] == st[1])break;
+                    else posCounter++;
+                }
+            }
+            else
+            {
+                for(int i=sp; i<=dPos; i++)
+                {
+                    if(s[i] == '.')break;
+                    else posCounter++;
+                }
+            }
+
+            if(posCounter >= 0 && posCounter <= 9) st[ind++] = '0';
+            pf("%s%d\n",st,posCounter);
+
+        }
+
+        memset(st,'\0',sizeof(st));
+
     }
 
-    else // IF there is a point
-    {
-        // copying data
-
-        cout << st << endl;
-
-    }
-
-
-
-
-     return 0;
+    return 0;
 }
