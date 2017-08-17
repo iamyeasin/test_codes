03522f1b2d85e52c4c61982d8bd368eda955648e
yeasin Rahaman <ryeashin@gmail.com>
2017-08-17 16:29:43 +0600

    Update u_dp.cpp

--- u_dp.cpp
+++ u_dp.cpp
@@ -4,99 +4,93 @@
 
 using namespace std;
 
-//vector < int > x[81];
+int bits[] = {0,32,16,8,4,2,1};
+bool h[64],m[64];
 
-int x[12][12];
-int pos1[] = {2,5,7};
-int fx[]={+1,-1,+0,+0};
-int fy[]={+0,+0,+1,-1};
+void print(){
+    pf(" ____________________________________________\n");
+    pf("|                                            |\n");
+    pf("|    ____________________________________    |_\n");
+    pf("|   |                                    |   |_)\n");
+    pf("|   |   8         4         2         1  |   |\n");
+    pf("|   |                                    |   |\n");
+    pf("|   |  ");
 
-bool cRow()
-{
-    int row[10];
-    for(int i=1; i<=9; i++)
-    {
-        for(int i=0; i<10; i++)row[i]=0;
 
-        for(int j=1; j<=9;j++)
-        {
-            if(!row[x[i][j]])row[x[i][j]] = 1;
-            else return false;
+    for(int i=3; i<=7; i++){
+        if(h[i]){
+            pf("o");
         }
+        else pf(" ");
+
+        if(i < 6 && i>2)pf("         ");
     }
-    return true;
+
+    pf("  |   |\n");
+    pf("|   |                                    |   |\n");
+    pf("|   |  ");
+
+
+    for(int i=1; i<=7; i++){
+        if(i<=2){
+            if(m[i]){
+                 pf(" o");
+
+            }
+            else pf("  ");
+        }
+        else{
+            if(m[i]){
+                pf("o");
+
+            }
+            else pf(" ");
+        }
+
+        if(i>=1 && i<7)pf("    ");
+    }
+
+
+    pf("  |   |\n");
+
+     pf("|   |                                    |   |\n");
+     pf("|   |   32    16    8     4     2     1  |   |_\n");
+     pf("|   |____________________________________|   |_)\n");
+     pf("|                                            |\n");
+     pf("|____________________________________________|\n");
+
+
+
+
+
 }
 
 
+int main(){
 
-bool cCol()
-{
-    int row[10];
-    for(int i=1; i<=9; i++)
-    {
-        for(int i=0; i<10; i++)row[i]=0;
+    long hh,mm;
+    while(sf("%d:%d",&hh,&mm) != EOF){
+        memset(m,0,sizeof(m));
+        memset(h,0,sizeof(h));
 
-        for(int j=1; j<=9;j++)
-        {
-            if(!row[x[j][i]])row[x[j][i]] = 1;
-            else return false;
-        }
-    }
-    return true;
-}
-
-bool isSub()
-{
-    int arr[11] = {0,0,0,0,0,0,0,0,0,0};
-
-    for(int i=0; i<3; i++)
-    {
-        memset(arr,0,sizeof(arr));
-        for(int j=0; j<=3; j++)
-        {
-            arr[pos1[j]] = 1;
-           for(int k=0; k<=3; k++)
-           {
-                int nx = pos1[i] + fx[k];
-                int ny = pos1[j] + fx[k];
-
-                if(!arr[x[nx][ny]])arr[x[nx][ny]] = 1;
-                else return false;
-           }
-        }
-    }
-
-    return true;
-}
-
-int main()
-{
-//    freopen("input.txt","rt",stdin);
-//    freopen("output.txt","wt",stdout);
-
-    int kase,y;
-    scanf("%d",&kase);
-
-    for(int i=1; i<=kase; i++)
-    {
-        for(int j=1; j<=9; j++)
-        {
-            for(int k=1; k<=9; k++)
-            {
-                int mm;
-                sf("%d",&mm);
-                if(mm >=1 && mm <=9)
-                {
-                    x[j][k] = mm;
-                }
+        for(int i=1; i<=7; i++){
+            if(hh >= bits[i]){
+                h[i] = 1; hh %= bits[i];
+            }
+            if(mm >= bits[i]){
+                m[i] = 1; mm %= bits[i];
             }
         }
+//
+//        for(int i=1; i<=6; i++){
+//            cout << pow(2,i) << "= " << m[i] <<endl;
+//        }
 
-        if(cRow() && cCol())pf("Instancia %d\nSIM\n\n",i);
-        else pf("Instancia %d\nNAO\n\n",i);
+        print();
+        puts("");
 
-        cout << isSub() << endl;
     }
+
 
 
     return 0;
