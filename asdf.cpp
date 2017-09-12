7c907848e3a720d616c9b773f9522e5f29a0bb8f
yeasin Rahaman <ryeashin@gmail.com>
2017-09-12 17:02:24 +0600

    Update asdf.cpp

--- asdf.cpp
+++ asdf.cpp
@@ -2,74 +2,56 @@
 
 using namespace std;
 
-vector < string > s;
+struct info{
+    char name[22];
+    int money=0;
+} ;
 
-int main(){
+int n;
+info arr[2005];
 
-    long n;
-    cin >> n;
-
-    //input taking
-    for(int i=0; i<n; i++){
-        string token;
-        cin >> token;
-        s.push_back(token);
+void addMoney(char *name, int money){
+    for(int i=1; i<=n; i++){
+        if(!strcmp(name,arr[i].name)){
+            arr[i].money += (money);
+        }
     }
-
-    long till; cin >> till;
-    for(int k=0; k<till; k++){
-        string test;
-        cin >> test;
-        int f=0;
-
-        //if it exists on the container
-        for(int i=0; i<n; i++){
-            if(s[i] == test){
-                cout << test << " is correct" << endl;
-                f=1;
-            }
-        }
-        if(f)continue;
-        int ok = 0;
-
-        int l1 = test.size();
-
-        for(int i=0; i<n; i++){
-            ok=1;
-            int l2 = s[i].size();
-            string gini = s[i];
-                if(l1 == l2){
-                    sort(gini.begin(),gini.end());
-                    sort(test.begin(),test.end());
-
-                    if(gini == test){
-                        cout << test << " is a misspelling of " << s[i] << endl;ok=1;
-                        break;
-                    }
-
-        }
-//        if(ok)continue;
-
-        int pro = 0;
-        for(int m=0; m<n; m++){
-            int l2 = s[m].size();
-            string ginis = s[m];
-
-            if(l1 >= l2 || (l2-1 == l1) || (l2+1 == l1) ){
-                sort(ginis.begin(),ginis.end());
-                sort(test.begin(),test.end());
-                pro =0;
-
-                for(int x=0; x<(max(l1,l2)); x++){
-                    if(ginis[x] != test[x]) pro++;
-                }
-                cout << pro << endl;
-                break;
-            }
-        }
-
-    }
-
 }
 
+
+int main(){
+    
+    while(scanf("%d",&n) != EOF){
+        for(int i=1; i<= n; i++){
+            scanf("%s",arr[i].name);
+        }
+    
+    int frns=0, amnt=0;
+    
+    for(int i=1; i <= n; i++){
+        char words[20];
+        scanf("%s %d %d",words,&amnt,&frns);
+            int each = (amnt/frns);
+            addMoney(words,(-1*amnt));
+            if((amnt - (each*frns) ) > 0) addMoney(word,(amnt - (each*frns)));
+            
+            for(int i=1; i<=frns; i++){
+                    char n[22];
+                   scanf("%s",n);
+                   addMoney(n,each);
+            }
+    }
+    
+    for(int i=1; i <= n; i++){
+        printf("%s %d\n",arr[i].name, arr[i].money);
+    }    
+    
+    
+    
+        
+    }
+    
+    
+    
+    return 0;    
 }
