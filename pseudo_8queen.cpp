469d9a242cc1d5e80c961843406c3caeddb63a05
yeasin Rahaman <ryeashin@gmail.com>
2017-10-08 12:41:16 +0600

    Update pseudo_8queen.cpp

--- pseudo_8queen.cpp
+++ pseudo_8queen.cpp
@@ -1,13 +1,25 @@
-/* bool conflict(int r, int c){ 
-     if(check row;
+ bool conflict(int r, int c){ 
+     if(check row; // optimize the technique
      check column;
      check LD, Check RD;
      ) return true;
      else return false;
+   }
+
+    void backtrack(int c){
+     if(c == n){
+          print bound;
+          return;
+     }    
+     
+     for(int r=0; r<n; r++){
+          if(board [r][c] != 'Q' && !conflict(r,c)){
+               board[r][c] = 'Q';
+               backtrack(c+1);
+               board[r][c] = '*';
+          }
+     }
+    
     }
 
-
-
-
-*/
 
