a33f834ae4f191ed0870043c52681e302ff292e9
yeasin Rahaman <ryeashin@gmail.com>
2017-10-09 15:42:55 +0600

    Update Pemutation_printing.cpp

--- Recursion Practice/Pemutation_printing.cpp
+++ Recursion Practice/Pemutation_printing.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 vector< string > vs;
 
-void recPermute(string soFar, string rest), string start{
+void recPermute(string soFar, string rest){
 
     if(rest.empty()){
         int vsz = vs.size(), flag = 0;
-        for(int i=0; i<vsz; i++){
+        for(int i=0; i<vsz; i++){ ///Storing all the unique string permutation in container
             if(vs[i] == soFar){
                 flag = 1;
                 break;
             }
         }
-        if(!flag){
+        if(!flag){ // If it is a unique string  store and print
             vs.push_back((soFar));
             cout << vs.size() << " " << soFar << endl;
         }else return;
@@ -21,7 +21,7 @@ void recPermute(string soFar, string rest), string start{
     }
     else {
         for(int i = 0; i < rest.length(); i++){
-            string remaining = rest.substr(0,i) + rest.substr(i+1);
+            string remaining = rest.substr(0,i) + rest.substr(i+1); // substr(0,0) returns null character.
 //            cout << "i= "<< i << " " << soFar+rest[i]<< "  " << rest.substr(0,i) << " p=" << rest.substr(i+1)  << " tmp = " << remaining << endl;
             recPermute(soFar+rest[i], remaining);
 
@@ -33,7 +33,7 @@ void recPermute(string soFar, string rest), string start{
 
 int main(){
 
-    recPermute("","abaaagh");
+    recPermute("","abaaagh"); // Yeah it also handles duplicates
 
 
     return 0;
